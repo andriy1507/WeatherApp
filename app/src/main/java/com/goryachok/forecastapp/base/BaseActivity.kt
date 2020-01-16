609d25d3d4115c7499ec9b194150b2c30ef37efd
Andriy Goryachok <andriy1507@hotmail.com>
2020-01-16 19:59:25 +0200

    Deleted unused files.

--- app/src/main/java/com/goryachok/forecastapp/base/BaseActivity.kt
+++
@@ -1,7 +0,0 @@
-package com.goryachok.forecastapp.base
-
-import androidx.appcompat.app.AppCompatActivity
-
-abstract class BaseActivity: AppCompatActivity(){
-    protected abstract fun setupDependencies()
-}
