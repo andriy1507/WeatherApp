c2a6ed37e3391586d52451057a558195fa137bc8
Andriy Goryachok <andriy1507@hotmail.com>
2020-01-31 17:38:26 +0200

    Fixed mapper.

--- repository/src/main/java/com/goryachok/repository/Mapper.kt
+++ repository/src/main/java/com/goryachok/repository/Mapper.kt
@@ -5,40 +5,35 @@ import com.goryachok.local.model.WeatherLocal
 import com.goryachok.remote.model.ForecastEntity
 import com.goryachok.remote.model.WeatherEntity
 
-class Mapper {
+private const val SECOND = 1000L
 
-    companion object {
-        private const val SECOND = 1000L
-    }
+fun WeatherEntity.mapWeatherToLocal(): WeatherLocal {
+    return WeatherLocal(
+        city = city,
+        temp = mainBodyRemote.temp,
+        humidity = mainBodyRemote.humidity,
+        pressure = mainBodyRemote.pressure,
+        windSpd = wind.speed,
+        windDir = wind.deg,
+        timeStamp = date * SECOND
+    )
+}
 
-    fun WeatherEntity.mapWeatherToLocal(): WeatherLocal {
-        return WeatherLocal(
-            city = city,
-            temp = mainBodyRemote.temp,
-            humidity = mainBodyRemote.humidity,
-            pressure = mainBodyRemote.pressure,
-            windSpd = wind.speed,
-            windDir = wind.deg,
-            timeStamp = date * SECOND
+fun ForecastEntity.mapForecastToLocal(): ForecastLocal {
+    val mapedList = mutableListOf<WeatherLocal>()
+    list.forEach {
+        mapedList.add(
+            WeatherLocal(
+                city = city.name,
+                description = it.weather.first().description,
+                temp = it.mainBodyRemote.temp,
+                humidity = it.mainBodyRemote.humidity,
+                pressure = it.mainBodyRemote.pressure,
+                windSpd = it.wind.speed,
+                windDir = it.wind.deg,
+                timeStamp = it.date * SECOND
+            )
         )
     }
-
-    fun ForecastEntity.mapForecastToLocal(): ForecastLocal {
-        val mapedList = mutableListOf<WeatherLocal>()
-        list.forEach {
-            mapedList.add(
-                WeatherLocal(
-                    city = city.name,
-                    description = it.weather.first().description,
-                    temp = it.mainBodyRemote.temp,
-                    humidity = it.mainBodyRemote.humidity,
-                    pressure = it.mainBodyRemote.pressure,
-                    windSpd = it.wind.speed,
-                    windDir = it.wind.deg,
-                    timeStamp = it.date * SECOND
-                )
-            )
-        }
-        return ForecastLocal(city.name, city.country, mapedList)
-    }
+    return ForecastLocal(city.name, city.country, mapedList)
 }
